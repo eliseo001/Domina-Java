ca6f91061026013cfa77de16dcf08f1ea0570720
eliseo001 <80852864+eliseo001@users.noreply.github.com>
2025-06-12 16:13:37 -0300

    Delete practica 1 directory

--- practica 1/Presentate.java
+++
@@ -1,7 +0,0 @@
-public class Presentate {
-    public static void main(String[] args) {
-        System.out.println("Mi nombre es Eliseo Masuelli");
-        System.out.println("Tengo 23 años");
-        System.out.println("Soy de Argentina");
-    }
-}
