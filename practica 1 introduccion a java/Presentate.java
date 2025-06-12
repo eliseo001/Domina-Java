ca4c551ce37913037cc77da9a55386d4d615e991
eliseo001 <80852864+eliseo001@users.noreply.github.com>
2025-06-12 16:14:27 -0300

    Introduccion a java
    "hola mundo"

---
+++ practica 1 introduccion a java/Presentate.java
@@ -0,0 +1,7 @@
+public class Presentate {
+    public static void main(String[] args) {
+        System.out.println("Mi nombre es Eliseo Masuelli");
+        System.out.println("Tengo 23 años");
+        System.out.println("Soy de Argentina");
+    }
+}
