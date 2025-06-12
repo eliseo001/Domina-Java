98d8e8bdc193aa42f35106ec576aa79ee3ab890f
eliseo001 <80852864+eliseo001@users.noreply.github.com>
2025-06-12 15:51:11 -0300

    "hola mundo con java"
    curso de java

---
+++ practica 1/Holamundo.java
@@ -0,0 +1,8 @@
+public class Holamundo {
+
+    public static void main (String[] args){
+        System.out.println("Hola mundo con java");
+        System.out.println("Saludos mi nombre es Eliseo");
+    }
+
+}
