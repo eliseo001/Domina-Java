1e42c8fb92da0f4fe79b6d414ee33e495bb51ab5
eliseo001 <80852864+eliseo001@users.noreply.github.com>
2025-11-06 13:31:19 -0300

    more clases

--- practica1/src/main/java/zona_fit/objetosDoc/Bicycle.java
+++ practica1/src/main/java/zona_fit/objetosDoc/Bicycle.java
@@ -4,17 +4,31 @@ public class Bicycle {
     private int cadence;
     private int speed;
     private int gear = 1;
+    private int id;
+    private static int numberOfBicycle = 0;
 
-    public Bicycle() {}
+    public Bicycle() {
+        // SOLUCIÓN: Añadir la asignación de ID
+        this.id = numberOfBicycle++;
+    }
 
     public Bicycle(int cadence, int speed, int gear) {
         this.cadence = cadence;
         this.speed = speed;
         this.gear = gear;
+        this.id = numberOfBicycle++;
+    }
+
+    public Bicycle(int startCadence, int startSpeed, int startGear, int startId) {
+        gear = startGear;
+        speed = startSpeed;
+        cadence = startCadence;
+        id = numberOfBicycle++;
     }
 
     void printStates(){
-        System.out.println("cadence:" +
+        System.out.println("id:" + id +
+                " cadence:" +
                 cadence + " speed:" +
                 speed + " gear:" + gear);
     }
@@ -56,5 +70,13 @@ public class Bicycle {
 
     public void setSpeed(int speed) {
         this.speed = speed;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public static int getNumberOfBicycle() {
+        return numberOfBicycle;
     }
 }
